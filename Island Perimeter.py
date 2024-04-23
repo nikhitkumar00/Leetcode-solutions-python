235056eccb5da2fe3b281709179106066846b256
Nikhit kumar <nikhitkumar00@gmail.com>
2024-04-23 19:38:24 +0530

    done

---
+++ Island Perimeter.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+
+        rows, cols = len(grid), len(grid[0])
+        directions = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        out = 0
+
+        for i in range(rows):
+            for j in range(cols):
+
+                if grid[i][j] == 1:
+                    perimeter = 4
+
+                    for row, col in directions:
+                        r, c = i + row, j + col
+
+                        if r in range(rows) and c in range(cols) and grid[r][c] == 1:
+                            perimeter -= 1
+
+                    out += perimeter
+
+        return out
