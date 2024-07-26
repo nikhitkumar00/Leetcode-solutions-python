25e86252723e5cc798c04c20fb6f805f1436e532
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-07-26 22:39:51 +0530

    done

--- Two sum.py
+++ Two sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         dic = {x: i for i, x in enumerate(nums)}
         for index, i in enumerate(nums):
             if target - i in dic and index != dic[target - i]:
