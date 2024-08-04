4423de33f9a48c3af3d5c277e99efc945460dba1
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-08-04 22:40:59 +0530

    Time: 351 ms (87.77%), Space: 36.4 MB (8.99%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,26 +1,32 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # Replace all negative numbers with 0, since we are only interested in positive numbers
-        for i in range(len(nums)):
-            if nums[i] < 0:
-                nums[i] = 0
 
-        # Use the index as a hash key to mark the presence of a number
-        for i in range(len(nums)):
-            val = abs(nums[i])
-            # Check if the value is within the range of the array
-            if 1 <= val <= len(nums):
-                # If the value at the indexed position is 0, mark it as negative to indicate presence
-                if nums[val - 1] == 0:
-                    nums[val - 1] = (len(nums) + 1) * -1
-                # If the value at the indexed position is positive, make it negative to indicate presence
-                if nums[val - 1] > 0:
-                    nums[val - 1] *= -1
+        sett = set(nums)
+        for i in range(1, len(nums) + 2):
+            if i not in sett:
+                return i
 
-        # Find the first index with a non-negative value, which indicates the first missing positive number
-        for i in range(len(nums)):
-            if nums[i] >= 0:
-                return i + 1
+        # # Replace all negative numbers with 0, since we are only interested in positive numbers
+        # for i in range(len(nums)):
+        #     if nums[i] < 0:
+        #         nums[i] = 0
 
-        # If no missing positive number is found within the range, return the length of the array + 1
-        return len(nums) + 1
+        # # Use the index as a hash key to mark the presence of a number
+        # for i in range(len(nums)):
+        #     val = abs(nums[i])
+        #     # Check if the value is within the range of the array
+        #     if 1 <= val <= len(nums):
+        #         # If the value at the indexed position is 0, mark it as negative to indicate presence
+        #         if nums[val - 1] == 0:
+        #             nums[val - 1] = (len(nums) + 1) * -1
+        #         # If the value at the indexed position is positive, make it negative to indicate presence
+        #         if nums[val - 1] > 0:
+        #             nums[val - 1] *= -1
+
+        # # Find the first index with a non-negative value, which indicates the first missing positive number
+        # for i in range(len(nums)):
+        #     if nums[i] >= 0:
+        #         return i + 1
+
+        # # If no missing positive number is found within the range, return the length of the array + 1
+        # return len(nums) + 1
