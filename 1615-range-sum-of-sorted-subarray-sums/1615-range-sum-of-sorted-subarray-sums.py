4dc486bf08a930bcf362f368cb00154fc4e785bb
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-08-04 11:31:56 +0530

    Time: 209 ms (82.74%), Space: 39.8 MB (60.62%) - LeetHub

---
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        
+        arr = []
+
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i,len(nums)):
+                current_sum += nums[j]
+                arr.append(current_sum)
+        
+        arr.sort()
+        return (sum(arr[left-1:right]))%1000000007
+        
