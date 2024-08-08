d27b886fc62d0668562a9b9746f21e139a2c654e
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-08-08 14:27:46 +0530

    Time: 81 ms (65.23%), Space: 17.9 MB (14.45%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.py
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.py
@@ -5,19 +5,20 @@ class Solution:
 
         directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
         row, col = rStart, cStart
-
-        out = []
+        out = [[rStart, cStart]]
         steps = 1
-        out.append([row, col])
         i = 0
+
         while len(out) < rows * cols:
 
-            for _ in range(2):
-                for _ in range(steps):
-                    row, col = row + directions[i][0], col + directions[i][1]
-                    if 0 <= row < rows and 0 <= col < cols:
-                        out.append([row, col])
-                i = (i + 1) % 4
-            steps += 1
+            dr, dc = directions[i]
+            for _ in range(steps):
+
+                row, col = row + dr, col + dc
+                if 0 <= row < rows and 0 <= col < cols:
+                    out.append([row, col])
+
+            i = (i + 1) % 4
+            steps += 1 if i % 2 == 0 else 0
 
         return out
