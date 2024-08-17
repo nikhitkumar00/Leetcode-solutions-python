fb7d03c3b24be3c4e32090f9b193228fb30b6c7c
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-08-17 15:38:52 +0530

    Time: 1540 ms (58.54%), Space: 51.1 MB (15.61%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -1,21 +1,19 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         ROWS, COLS = len(points), len(points[0])
-        left, right = [0] * COLS, [0] * COLS
-        row = points[0]
+        dp = points[0]
 
         for i in range(1, ROWS):
-            left[0] = row[0]
-            right[-1] = row[-1]
+            left, right = [0] * COLS, [0] * COLS
+            left[0] = dp[0]
+            right[-1] = dp[-1]
 
             for l in range(1, COLS):
-                left[l] = max(row[l], left[l - 1] - 1)
+                left[l] = max(dp[l], left[l - 1] - 1)
 
             for r in range(COLS - 2, -1, -1):
-                right[r] = max(row[r], right[r + 1] - 1)
+                right[r] = max(dp[r], right[r + 1] - 1)
 
-            for j in range(COLS):
-                row[j] = max(left[j], right[j]) + points[i][j]
-            print(row)
+            dp = [max(left[j], right[j]) + points[i][j] for j in range(COLS)]
 
-        return max(row)
+        return max(dp)
