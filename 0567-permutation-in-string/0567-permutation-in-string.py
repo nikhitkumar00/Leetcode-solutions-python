4892f26d31edea6515fd2f7fdf55bc7a9ddab6c8
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-10-05 10:55:20 +0530

    Time: 43 ms (97.59%), Space: 16.6 MB (62.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,7 +1,22 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        c1 = Counter(s1)
-        for i in range(len(s2) - len(s1) + 1):
-            if c1 == Counter(s2[i : i + len(s1)]):
+        c1, c2 = [0] * 26, [0] * 26
+
+        if len(s1) > len(s2):
+            return False
+
+        for i in range(len(s1)):
+            c1[ord(s1[i]) - 97] += 1
+            c2[ord(s2[i]) - 97] += 1
+
+        if c1 == c2:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            c2[ord(s2[i]) - 97] += 1
+            c2[ord(s2[i - len(s1)]) - 97] -= 1
+
+            if c1 == c2:
                 return True
+
         return False
