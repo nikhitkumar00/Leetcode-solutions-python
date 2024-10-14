478f0aad9d34582f556d9cbe38355764ed49d8fc
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-10-14 11:29:20 +0530

    Time: 666 ms (94.62%), Space: 31.3 MB (20.57%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.py
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.py
@@ -3,11 +3,8 @@ class Solution:
         score = 0
         nums = [-i for i in nums]
         heapq.heapify(nums)
-        print(nums)
 
         for i in range(k):
-            val = -heapq.heappop(nums)
-            score += val
-            heapq.heappush(nums, -ceil(val / 3))
+            score -= heapq.heappushpop(nums, floor(nums[0] / 3))
 
         return score
