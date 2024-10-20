2f4b6abf66340557bfeffa3ad304fbb515ebeb1e
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-10-20 12:52:41 +0530

    Time: 71 ms (16.17%), Space: 22.5 MB (5.39%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
@@ -9,10 +9,19 @@ class Solution:
         def parsenot(expr):
             return not expr
 
-        return eval(
-            expression.replace("t", "True")
-            .replace("f", "False")
-            .replace("&", "parseand")
-            .replace("|", "parseor")
-            .replace("!", "parsenot")
-        )
+        s = []
+        for c in expression:
+            if c == "t":
+                s.append("True")
+            elif c == "f":
+                s.append("False")
+            elif c == "&":
+                s.append("parseand")
+            elif c == "|":
+                s.append("parseor")
+            elif c == "!":
+                s.append("parsenot")
+            else:
+                s.append(c)
+
+        return eval("".join(s))
