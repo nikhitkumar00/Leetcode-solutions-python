7eecc73101a71997c3953133591e8d8839bb3d52
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-11-24 12:04:34 +0530

    Time: 90 ms (53.85%), Space: 25 MB (72.1%) - LeetHub

---
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        total = 0
+        negative_count = 0
+        min_val = inf
+
+        for row in matrix:
+            for val in row:
+                total += abs(val)
+                if val < 0:
+                    negative_count += 1
+                min_val = min(min_val, abs(val))
+
+        if negative_count % 2 == 1:
+            total -= 2 * min_val
+        return total
