6440c00be46eecfe05ad25ff340bdd18a722e9cd
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-11-26 11:31:09 +0530

    Time: 18 ms (22.52%), Space: 17.8 MB (44.02%) - LeetHub

---
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findChampion(self, n, edges):
+        l = set(i[0] for i in edges)
+        r = set(i[1] for i in edges)
+
+        out = -1
+        for i in range(n):
+            if i not in r:
+                if out == -1:
+                    out = i
+                else:
+                    return -1
+        return out
