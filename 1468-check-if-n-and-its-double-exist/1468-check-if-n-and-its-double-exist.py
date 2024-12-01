7cb33ba16606b7e4a1e1586788bdc9cda6fb858f
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-12-01 09:18:51 +0530

    Time: 1 ms (63.83%), Space: 17.5 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,15 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        hash = set(arr)
+        hash = set()
         for i in arr:
-            if 2 * i in hash:
-                if i == 0 and arr.count(0) > 1:
-                    return True
-                elif i != 0:
-                    return True
-            if i % 2 == 0 and i / 2 in hash:
-                if i == 0 and arr.count(0) > 1:
-                    return True
-                elif i != 0:
-                    return True
+            if 2 * i in hash or (i % 2 == 0 and i // 2 in hash):
+                return True
+            hash.add(i)
         return False
