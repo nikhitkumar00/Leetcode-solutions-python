d000b4f35c8a95049090bad9dadd5b356adfc5a8
Nikhit Kumar N S <nikhitkumar00@gmail.com>
2024-12-07 10:58:05 +0530

    Time: 743 ms (43.69%), Space: 29.4 MB (19.09%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
@@ -3,14 +3,12 @@ class Solution:
         l, r = 1, max(nums)
         while l < r:
             m = (l + r) // 2
-            canDivide = True
             ops = 0
             for num in nums:
                 ops += ceil(num / m) - 1
                 if ops > maxOperations:
-                    canDivide = False
                     break
-            if canDivide:
+            if ops <= maxOperations:
                 r = m
             else:
                 l = m + 1
